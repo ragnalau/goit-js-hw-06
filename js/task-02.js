4d8d66548b7af71195b787bee4cc7bd7f96a3ffc
Reut Mihai <reutmihai@icloud.com>
2024-08-01 19:22:35 +0300

    task 2 solve
    create a list with DOM

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsListEl = document.getElementById("ingredients");
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.textContent = ingredient;
+  ingredientsListEl.append(listItem);
+});
+
+console.log(ingredientsListEl);
