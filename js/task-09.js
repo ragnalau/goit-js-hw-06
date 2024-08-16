d87881c5d0033a03584343eec409bc3b680c500f
Reut Mihai <reutmihai@icloud.com>
2024-08-16 11:19:44 +0300

    task 9 solved

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
+'use strict'
+/*
+Scrieți un script care schimbă culorile de fundal al elementului <body>, 
+prin stiluri inline, atunci când se face click pe button.change-color și 
+afișați valoarea culorii în span.color.
+*/
+
+const changeColorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+changeColorBtn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+  
+})
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
